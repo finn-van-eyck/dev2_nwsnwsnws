f3ea218e3f4af8a824d55e83321bb056808663c2
Finn Van Eyck <finn.vaneyck@student.arteveldehs.be>
2026-03-08 17:02:38 +0100

    vercel toegevoegd

--- server/app.ts
+++ server/app.ts
@@ -4,7 +4,7 @@ import expressLayouts from "express-ejs-layouts";
 import routes from "./routes";
 
 const app: Application = express();
-const PORT: number = 3000;
+const PORT: number = parseInt(<string>process.env.PORT, 10) || 3000;
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
